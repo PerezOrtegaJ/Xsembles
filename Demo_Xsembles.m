1f843a25ae26ff3944c2821761a2c457c51129a5
Jesús Pérez-Ortega <jesus.epo@gmail.com>
2022-08-30 14:26:53 -0400

    Update Demo_Xsembles.m

--- Demo_Xsembles.m
+++ Demo_Xsembles.m
@@ -3,8 +3,8 @@
 % Load a raster example from experimental data
 load raster_example.mat
 
-% Get ensembles from raster
+% Search for ensembles and offsembles from the raster
 analysis = Get_Xsembles(raster);
 
-% Plot raster with ensembles identified
-Plot_Ensemble_Raster(analysis)
+% Plot raster with the ensemble activity identified
+Plot_Raster_Ensemble_Activity(analysis)
